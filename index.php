b60d01ebed19f16bfbe1160dd6786c0d9f8399f3
Konstantin Nagula <konstantin.nagula@codeit.pro>
2017-03-09 11:42:39 +0200

    error logs

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 
+/**
+ * Set display errors
+ */
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
+/**
+ * @var string $LogsContent Erorr logs
+ */
+$LogsContent = file_get_contents('Logs/errors.log');
